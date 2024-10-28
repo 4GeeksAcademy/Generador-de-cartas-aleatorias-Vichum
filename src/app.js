fbe9d0d1ba97e6f144622ba4301a76d255db7b1f
Vichum <100027914+ElVichum@users.noreply.github.com>
2024-10-28 19:50:28 +0000

    generador listo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const valores = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const palos = [
+    '<i class="fa-solid fa-heart"></i>',
+    '<i class="fa-solid fa-clover"></i>',
+    '<i class="fa-solid fa-spa"></i>',
+    '<i class="fa-solid fa-diamond"></i>'
+  ];
+
+  // Función para generar una carta aleatoria
+  function generarCarta() {
+    const valor = valores[Math.floor(Math.random() * valores.length)];
+    const palo = palos[Math.floor(Math.random() * palos.length)];
+
+    document.getElementById("valor").innerHTML = valor;
+    document.getElementById("palo").innerHTML = palo;
+  }
+
+  generarCarta();
 };
